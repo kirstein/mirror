ee7259359f28c89a70da86a7a6e3ef4d55e5eaa9
Mikk Kirstein <mikk.kirstein@zerotech.ee>
2012-04-03 15:45:28 +0300

    added forgotten exception

---
+++ src/main/java/org/kolmas/mirror/exception/MirrorCantFindMethodException.java
@@ -0,0 +1,17 @@
+package org.kolmas.mirror.exception;
+
+public class MirrorCantFindMethodException extends RuntimeException {
+
+    private static final long serialVersionUID = -7663746462541965549L;
+    public static final String METHOD_NOT_FOUND = "Container method with @ContainMethod annotation and store or receive parameter was not found.";
+    public static final String STORE_METHOD_NOT_ACCEPTABLE = "Container method with @ContainMethod(store=\"%s\") annotation has invalid parameters or is not public. Method must be public void and its parameters must be 'methodName(String key, Class<?> type, Object result)'";
+    public static final String RETRIEVE_METHOD_NOT_ACCEPTABLE = "Container method with @ContainMethod(retrieve=\"%s\") annotation has invalid parameters or is not public. Method must be public object and its parameters must be 'methodName(String key)'";
+
+    public MirrorCantFindMethodException(String ex, String methodname, Throwable e) {
+        super(String.format(ex, methodname), e);
+    }
+
+    public MirrorCantFindMethodException(String methodname) {
+        super(String.format(METHOD_NOT_FOUND, methodname));
+    }
+}
